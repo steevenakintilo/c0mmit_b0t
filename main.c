e9e708252d9f7e22022c0697bda5026beeec6da7
AKINTILO Steeven <Steeven@localhost.localdomain>
2021-05-30 14:44:39 +0200

    rrr

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int add_int(int x, char  const *file)
     struct stat size;
     stat(file, &size);
     int fd = open(file, O_RDONLY);
-    char *buffer = malloc(sizeof(char) *(size.st_size + 1));
+    65;6003;1c    char *buffer = malloc(sizeof(char) *(size.st_size + 1));
     read(fd, buffer, size.st_size);
     buffer[size.st_size] = '\0';
     int res = my_getnbr(buffer) - x;
